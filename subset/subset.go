d8bbd5056d5e0e25edd962ed90d07c5c6b368c89
joerg kost <jk@ip-clear.de>
2017-12-27 17:15:22 +0100

    - cleanup - added subset

--- subset/subset.go
+++ subset/subset.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	generateSubsets([]int{}, []int{1,2,3})
+	generateSubsets([]int{}, []int{1,2})
+	generateSubsetsFromBitset([]int{1,2,3})
 }
 
 func generateSubsets(far, left []int) {
@@ -13,4 +17,18 @@ func generateSubsets(far, left []int) {
 		generateSubsets(append(far, left[0]), left[1:])
 		generateSubsets(far, left[1:])
 	}
+}
+
+func generateSubsetsFromBitset(subset []int) {
+	y := math.Pow(2, float64(len(subset)))
+
+	for i := 0; i < int(y); i++ {
+		for j := uint32(0); j < uint32(len(subset)); j++ {
+			if uint32(i) & (1<<j) > 0 {
+				fmt.Printf("%d ", subset[j])
+			}
+		}
+		fmt.Println("")
+	}
+
 }
